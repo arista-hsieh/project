2042ebc7a06bdc8c4d7a81ebcd2efaecbd0427eb
Arista <arista.hsieh@gmail.com>
2022-11-23 07:40:30 +0800

    Update 2022-11-22T23:40:26.612Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,10 +1,2 @@
 "use strict";
-
-$(function () {
-  $(".navBar").hide();
-  $(".fa-bars").click(function () {
-    $(".fa-bars").toggleClass('fa-xmark');
-    $(".navBar").slideToggle();
-  });
-});
 //# sourceMappingURL=all.js.map
